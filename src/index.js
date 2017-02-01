f95fdc3563ee3d5a108bc9ae6f263fb53b2115f6
Ian Morton <ian.morton@sky.uk>
2017-02-01 18:46:03 +0000

    Trying for common header

--- src/index.js
+++ src/index.js
@@ -1,45 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory} from 'react-router'
+import { Router, Route, browserHistory } from 'react-router'
+
+import Header from './Header';
 import App from './App';
 import Two from './Two';
 import './index.css';
 import * as firebase from 'firebase';
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBEV_nobmOnrlLgx4dq34U5X2ZeV7H4BNE",
-    authDomain: "draw-6bba6.firebaseapp.com",
-    databaseURL: "https://draw-6bba6.firebaseio.com",
-    storageBucket: "draw-6bba6.appspot.com",
-    messagingSenderId: "393848162065"
-  };
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBEV_nobmOnrlLgx4dq34U5X2ZeV7H4BNE",
+  authDomain: "draw-6bba6.firebaseapp.com",
+  databaseURL: "https://draw-6bba6.firebaseio.com",
+  storageBucket: "draw-6bba6.appspot.com",
+  messagingSenderId: "393848162065"
+};
 //  firebase.initializeApp(config);
 
-const fb = firebase  
+const fb = firebase
   .initializeApp(config)
   .database()
   .ref();
 
 // Add some "action" functions
 // These will update our firebase database
-const addLocation = data => fb.child('locations').push(data, response => response);  
-const updateLocation = (id, data) => fb.child(`locations/${id}`).update(data, response => response);  
-const addTeam = data => fb.child('teams').push(data, response => response);  
-const updateTeam = (id, data) => fb.child(`teams/${id}`).update(data, response => response);  
-const actions = {  
+const addLocation = data => fb.child('locations').push(data, response => response);
+const updateLocation = (id, data) => fb.child(`locations/${id}`).update(data, response => response);
+const addTeam = data => fb.child('teams').push(data, response => response);
+const updateTeam = (id, data) => fb.child(`teams/${id}`).update(data, response => response);
+const actions = {
   addLocation,
   updateLocation,
   addTeam,
   updateTeam
 };
 
-fb.on('value', snapshot => {  
+fb.on('value', snapshot => {
   const store = snapshot.val();
   ReactDOM.render(
     <Router history={browserHistory}>
-      <Route path="/" component={() => (<App {...actions}{...store} />)} />
-      <Route path="/two" component={() => (<Two {...store} />)} />
+      <Route path="/" component={Header}>
+        <Route path="/" component={() => (<App {...actions}{...store} />)} />
+        <Route path="/two" component={() => (<Two {...store} />)} />
+      </Route>
     </Router>,
     document.getElementById('root')
   );
